332102ccb5a07fc6e25c1229ced91e602b29e003
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2024-03-19 11:09:32 +0700

    Update 2-aplikasi-note.py

--- Pertemuan 3/2-aplikasi-note.py
+++ Pertemuan 3/2-aplikasi-note.py
@@ -24,7 +24,8 @@ class FormCatatan(UserControl) :
                     #tombol tambah data
                     FloatingActionButton(
                         icon= icons.ADD,
-                        bgcolor= "purple"
+                        bgcolor= "purple",
+                        on_click=catatan.tambah_catatan
                     )
                 ]
             ),
@@ -35,6 +36,9 @@ class FormCatatan(UserControl) :
 
     #fungsi perintah tambah data
     def tambah_catatan (catatan, e) :
+        data_catatan_baru = FormDataCatatan(catatan.inputan_catatan_baru.value, catatan.hapus_catatan)
+        catatan.layout_data.controls.append(data_catatan_baru)
+
         catatan.inputan_catatan_baru.value = ""
         catatan.update()
 
@@ -43,44 +47,38 @@ class FormCatatan(UserControl) :
         catatan.layout_data_controls.remove(data_catatan_masuk)
         catatan.update()
 
-    #fungsi perintah ubah data
-    def ubah_catatan (catatan, e) :
-        catatan.inputan_catatan_ubah.value = catatan.data_catatan.label
-        catatan.tampil_data.visible = False
-        catatan.tampil_ubahdata.visible = True
-        catatan.update()
-
+# membuat class form data rekapan
 class FormDataCatatan(UserControl) :
-    def __init__(catatan, isi_catatan, hapus_catatan):
-        super().__init__()
-        catatan.isi_catatan = isi_catatan
-        catatan.hapus_catatan = hapus_catatan
+        def __init__(catatan, isi_catatan, hapus_catatan):
+            super().__init__()
+            catatan.isi_catatan = isi_catatan
+            catatan.hapus_catatan = hapus_catatan
 
-    def build(catatan):
-        catatan.data_catatan = Checkbox(value=False, label=catatan.isi_catatan)
+        def build(catatan):
+            catatan.data_catatan = Checkbox(value=False, label=catatan.isi_catatan)
 
-        catatan.tampil_data = Row(
-            alignment="spaceBetween",
-            VerticalAlignment="center",
-            controls=[
-                catatan.data_catatan,
-                Row(
-                    spacing=0,
-                    controls=[
-                        IconButton(
-                            icon=icons.CREATE_OUTLINED,
-                            tooltip = "ubah",
-                        ),
-                        IconButton(
-                            icons.DELETE_OUTLINE,
-                            tooltip = "hapus",
-                        ),
-                    ],
-                ),
-            ],
-        )
+            catatan.tampil_data = Row(
+                alignment="spaceBetween",
+                vertical_alignment="center",
+                controls=[
+                    catatan.data_catatan,
+                    Row(
+                        spacing=0,
+                        controls=[
+                            IconButton(
+                                icon=icons.CREATE_OUTLINED,
+                                tooltip = "ubah",
+                            ),
+                            IconButton(
+                                icons.DELETE_OUTLINE,
+                                tooltip = "hapus",
+                            ),
+                        ]
+                    )
+                ]
+            )
 
-        return Column(controls=[catatan.tampil_data])
+            return Column(controls=[catatan.tampil_data])
 
 #function/fungsi utama
 def main (page : Page):
